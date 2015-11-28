49636941a3bb74e180c4c3afa0a18bec0386277f
Iliiazbek Akhmedov <ssymbol.1993@gmail.com>
2015-11-28 13:23:51 -0800

    reading from file

--- main.cu
+++ main.cu
@@ -23,13 +23,54 @@ int block_size = 1024; //1025 max (# of threads in a block)
 
 int main(int argc, char *argv[]) {
 
-    int atoms_cnt = 200000;
-    int workload = 10;
+    /**
+    * Read the number of particles
+    */
+    int numOfParticles = argv[1];
 
-    atoms_cnt = atoi(argv[1]);
-    workload = atoi(argv[2]);
+    /**
+    * Read name of the file
+    */
+    string fileName = argv[2];
+    ifstream fileStream(fileName.c_str());
+    cout << "Reading file: " << fileName << endl;
 
-    run_single_kernel(atoms_cnt, workload);
+    atom* atoms = (atom*)malloc(sizeof(atom) * numOfParticles);
+
+    int heads = 0;
+    int atomCount = 0;
+
+    string token;
+    while(stream >> token) {
+        if(token.compare("HEAD") == 0) {
+            heads++;
+            atomCount = 0;
+        } else if(token.compare("ATOM")) {
+            //example: `ATOM  00000000    00000001    00000001    17.297  15.357  5.428   -0.548  15.9994`
+
+            //frame (just skipping for now)
+            stream >> token;
+            //number
+            stream >> atoms[atomCount].id;
+            //type (just skipping for now)
+            stream >> token;
+            stream >> atoms[atomCount].x;
+            stream >> atoms[atomCount].y;
+            stream >> atoms[atomCount].z;
+            stream >> atoms[atomCount].charge;
+            stream >> atoms[atomCount].mass;
+
+            atomCount++;
+        }
+    }
+
+    // int atoms_cnt = 200000;
+    // int workload = 10;
+
+    // atoms_cnt = atoi(argv[1]);
+    // workload = atoi(argv[2]);
+
+    // run_single_kernel(atoms_cnt, workload);
 
     // check for error
     cudaError_t error = cudaGetLastError();
