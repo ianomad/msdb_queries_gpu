3e0fafb06ee63ccb60d9e209c6144a1e38fd7cdd
Iliiazbek Akhmedov <ssymbol.1993@gmail.com>
2016-06-05 15:52:27 -0700

    automatic triggered changes

--- gpu_single.cu
+++ gpu_single.cu
@@ -254,8 +254,6 @@ void run_single_kernel(int atomsCnt, atom* atomList, int workload) {
     //int gridSize = ceil(atomsCnt / (float)blockSize) + 1;
     //int stripe = 1024 / ;
 
-    printf("Histogram size: %d\n", num_buckets * sizeof(unsigned long long));
-
     int i, w;
     for(w = 0; w < workload; w++) {
 
@@ -286,7 +284,8 @@ void run_single_kernel(int atomsCnt, atom* atomList, int workload) {
         gpu_one_body_functions_kernel<<<grid_size, block_size, smem1, streamComp1 >>>(g_s_atomsCnt, g_s_atom_list, g_s_res);
 
         //----------------------------------2 BODY KERNEL---------------------------------------------------
-        int smem2 = num_buckets * sizeof(unsigned long long) + 2 * block_size.x * sizeof(atom);
+        int smem2 = num_buckets * sizeof(unsigned long long) + 3 * block_size.x * sizeof(atom);
+        printf("SMEM size: %d\n", smem2);
         gpu_two_body_functions_kernel<<<grid_size, block_size, smem2, streamComp2 >>>(g_s_atom_list, atomsCnt, d_histogram, num_buckets, PDH_res);
         
         cudaStreamSynchronize(streamComp1);
