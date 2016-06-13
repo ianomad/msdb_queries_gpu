a04d919443ff8338229e4158105f7fdc2ead46c3
Iliiazbek Akhmedov <ssymbol.1993@gmail.com>
2016-06-12 17:16:53 -0700

    adjusting number of buckets

--- gpu_single.cu
+++ gpu_single.cu
@@ -216,7 +216,7 @@ void output_histogram(bucket* hist, int num_buckets) {
 
 void run_single_kernel(int atomsCnt, atom* atomList, int workload, float bucket_width, float space) {
 
-    int num_buckets = space / bucket_width + 1;
+    int num_buckets = (int) (space * space * 3 / bucket_width) + 1;
 
     query_results* res = (query_results*) malloc(sizeof(query_results));
     bucket* histogram = (bucket *)malloc(sizeof(bucket) * num_buckets); 
