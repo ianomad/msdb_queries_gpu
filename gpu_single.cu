5d466c82c3c9b07e1f99ae175374af50a5aa2ea7
Iliiazbek Akhmedov <ssymbol.1993@gmail.com>
2016-06-12 18:33:42 -0700

    automatic triggered changes

--- gpu_single.cu
+++ gpu_single.cu
@@ -213,11 +213,11 @@ void gpu_two_body_functions_kernel(atom* at_list, int PDH_acnt, bucket* hist, in
 
     __syncthreads();
 
-    // if(threadIdx.x == 0 && histogram_in_sm) {
-    //     for(i = 0; i < num_buckets; i++) {
-    //         atomicAdd(&hist[i].d_cnt, shared_histo[i]);
-    //     }
-    // }
+    if(threadIdx.x == 0 && histogram_in_sm) {
+        for(i = 0; i < num_buckets; i++) {
+            atomicAdd(&hist[i].d_cnt, shared_histo[i]);
+        }
+    }
 }
 
 void output_histogram(bucket* hist, int num_buckets) {
